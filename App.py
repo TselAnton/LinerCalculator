83dafb8ed1408bb927a30164fb57bf2e4930a014
Anton_Tsel <anton_tsel@epam.com>
2020-04-15 11:42:35 +0400

    Add generate solution func in App

--- App.py
+++ App.py
@@ -2,12 +2,12 @@ from api.GeneratorUtils import GeneratorUtils
 from api.BranchAndBorderMethod import BranchAndBorderMethod
 
 
-def generate_solution(num_of_args, isMax):
+def generate_solution(num_of_args):
     is_normal = False
     while not is_normal:
         fun, borders = GeneratorUtils.generate_liner_condition(num_of_args)
 
-        result, time, depth = BranchAndBorderMethod.find_solution_with_all_params(fun, borders, isMax, False, False, num_of_args + 5)
+        result, time, depth = BranchAndBorderMethod.find_solution_with_all_params(fun, borders, True, False, False, num_of_args + 5)
         if depth >= num_of_args:
             print("На максимум:")
             print("Один поток: ", BranchAndBorderMethod.find_solution_with_time(fun, borders, True, False))
@@ -19,4 +19,4 @@ def generate_solution(num_of_args, isMax):
             is_normal = True
 
 
-generate_solution(9, True)  # True - max, False - min
+generate_solution(9)  # Номер — количество переменных x
